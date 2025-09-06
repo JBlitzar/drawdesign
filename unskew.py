1f4f561c8e113f587af75ed580ea06b6f8129eda
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-09-06 12:43:46 -0700

    redo in python

---
+++ unskew.py
@@ -0,0 +1,97 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "numpy",
+#     "opencv-python",
+#     "pillow",
+# ]
+# ///
+import cv2
+import numpy as np
+from PIL import Image
+import itertools
+
+
+def save(img, name="out.png"):
+    im = Image.fromarray(img)  # (img * 255).astype(np.uint8)
+    im.save(name)
+
+
+img = np.array(Image.open("test.png"))
+print(img.shape)
+
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+gray = gray / np.max(gray) * 255
+
+
+gray = (gray > 160) * 255
+
+gray = gray.astype(np.uint8)
+
+save(gray, "gray-init.png")
+
+kernel = np.ones((5, 5), np.uint8)
+gray = cv2.dilate(gray, kernel, iterations=2)
+gray = cv2.erode(gray, kernel, iterations=2)
+
+save(gray, "gray.png")
+
+
+# from https://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function
+def intersects(a, b, c, d, p, q, r, s):
+    det = (c - a) * (s - q) - (r - p) * (d - b)
+    if det == 0:
+        return False
+    else:
+        lambda_ = ((s - q) * (r - a) + (p - r) * (s - b)) / det
+        gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det
+        return (0 < lambda_ < 1) and (0 < gamma < 1)
+
+
+# lenght of line, implemented by me
+def length(a, b, c, d):
+    dy = d - b
+    dx = c - a
+    return np.sqrt(dy**2 + dx**2)
+
+
+contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+if contours:
+    cnt = max(contours, key=cv2.contourArea)
+
+    epsilon = 0.02 * cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyN(cnt, 4, epsilon_percentage=epsilon, ensure_convex=True)
+    corners = approx.reshape(-1, 2)
+
+    mask = np.zeros_like(gray)
+    cv2.drawContours(mask, [cnt], -1, 255, -1)
+    for c in corners:
+        cv2.circle(mask, tuple(c), 50, 120, -1)
+
+    gray = mask
+
+    assert len(corners) == 4
+
+    for ordering in itertools.permutations(corners):
+        # if AB intersects CD
+        o = ordering
+        a = o[0]
+        b = o[1]
+        c = o[2]
+        d = o[3]
+        if intersects(a[0], a[1], b[0], b[1], c[0], c[1], d[0], d[1]) and length(
+            a[0], a[1], c[0], c[1]
+        ) < length(b[0], b[1], c[0], c[1]):
+            # probably good! corners intersect.
+            corners = [a, c, b, d]
+            break
+
+    size = (int(8.5 * 100), int(11 * 100))
+    pts2 = np.float32([[0, 0], [size[0], 0], [size[0], size[1]], [0, size[1]]])
+    # https://math.stackexchange.com/questions/2789094/deskew-and-rotate-a-photographed-rectangular-image-aka-perspective-correction
+    M, mask = cv2.findHomography(np.float32(corners), pts2)
+
+    dst = cv2.warpPerspective(img, M, size)
+
+    save(dst, "unskewed.png")
