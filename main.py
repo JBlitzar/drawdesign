18c092d32946f30d8ee46d8fcd581a44a0c20556
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-09-06 14:22:19 -0700

    prints website code out given image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, send_from_directory
+import base64
+from skewed_to_website import get_website_from_camera_feed_image
 
 app = Flask(__name__)
 
@@ -6,6 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
+
+
+@app.route("/snapshot", methods=["POST"])
+def image_snapshot():
+    img_b64 = request.json["image_data"].split(",")[1]
+    imgdata = base64.b64decode(img_b64)
+    with open("photo.jpg", "wb") as f:
+        f.write(imgdata)
+
+    print(get_website_from_camera_feed_image("photo.jpg"))
 
 
 if __name__ == "__main__":
